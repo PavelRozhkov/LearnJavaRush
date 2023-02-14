d4461d7fb77a45e1fd8a3cbca3f7c3948a84c398
Павел Р <grvsend@gmail.com>
2023-02-14 19:11:21 +0200

    20230214 1911

--- src/Spring/src/main/java/Level0/Greeting.java
+++ src/Spring/src/main/java/Level0/Greeting.java
@@ -1,5 +1,45 @@
 package Level0;
-public record Greeting(long id, String content) {
+
+import java.util.Objects;
+
+public final class Greeting {
+    private final long id;
+    private final String content;
+
+    Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long id() {
+        return id;
+    }
+
+    public String content() {
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        Greeting that = (Greeting) obj;
+        return this.id == that.id &&
+                Objects.equals(this.content, that.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, content);
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting[" +
+                "id=" + id + ", " +
+                "content=" + content + ']';
+    }
+
     /**
      * Объект Greetingдолжен быть преобразован в JSON. Благодаря поддержке преобразователя
      * сообщений HTTP в Spring вам не нужно выполнять это преобразование вручную. Поскольку
--- src/Spring/src/main/java/guide_rest/PayrollApplication.java
+++ src/Spring/src/main/java/guide_rest/PayrollApplication.java
@@ -2,10 +2,14 @@ package guide_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * https://spring.io/guides/tutorials/rest/
+ * Создание сервисов REST с помощью Spring
+ * https://github.com/spring-guides/tut-rest --> D:\dev\2023.2\tut-rest\pom.xml
+ * Работает: в поме нужна версия spring-boot-starter-parent 2.3.0.RELEASE и версия байткода 1.8
+ */
 @SpringBootApplication
 public class PayrollApplication {
-
     public static void main(String... args) {
         SpringApplication.run(PayrollApplication.class, args);
     }
